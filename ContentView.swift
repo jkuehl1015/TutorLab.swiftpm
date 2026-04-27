9808d7a4d2601496484dd41e9985fcbea9c88028
Jack Kuehl <jkuehl1015@gmail.com>
2026-04-27 14:34:47 -0500

    I added a title

--- ContentView.swift
+++ ContentView.swift
@@ -2,11 +2,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text("Welcome To TutorLab")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+            }
         }
     }
 }
