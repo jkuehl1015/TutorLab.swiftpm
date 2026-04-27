69c6c8f0007291c05e02973123c61283706535d4
Jack Kuehl <jkuehl1015@gmail.com>
2026-04-27 15:08:12 -0500

    added class

---
+++ Save.swift
@@ -0,0 +1,19 @@
+//
+//  Save.swift
+//  TutorLab
+//
+//  Created by Jack L. Kuehl on 4/27/26.
+//
+import SwiftUI
+
+class Save {
+    var studentID: String
+    var studentName: String
+    var studentTeacher: String
+    
+    init() {
+        self.studentID = ""
+        self.studentName = ""
+        self.studentTeacher = ""
+    }
+}
